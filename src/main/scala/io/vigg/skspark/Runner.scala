ffe9a6de77f22fbeca8a71b03a3610838fdd9745
andrewmueller <Muelleau1917@gmail.com>
2022-04-20 20:54:54 -0400

    runner job parsing

--- src/main/scala/io/vigg/skspark/Runner.scala
+++ src/main/scala/io/vigg/skspark/Runner.scala
@@ -6,15 +6,22 @@ object Runner {
       args: Array[String]
   ): SkApplication = {
 
-    case "test" => {
-      new SparkJob {
-        override def run: Unit = println("run test")
-        override def setConf: Unit = {}
+    val job = args(0)
+
+    job match {
+
+      case "test" => {
+        new SparkJob {
+          override def run: Unit = println("run test")
+          override def setConf: Unit = {}
+        }
       }
+
+      case _ =>
+        println(job)
+        throw new Exception("invalid job id provided")
+
     }
-    case _ =>
-      println(job)
-      throw new Exception("invalid job id provided")
 
   }
 
